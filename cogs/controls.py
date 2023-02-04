d4dbc8bd5f3090bfe808e1c3e97e8bf05cf4fd7a
Codanaut <cody@codanaut.com>
2023-02-04 03:05:23 -0700

    moved leave command to controls cog.
    All playback controls will move to this file.

---
+++ cogs/controls.py
@@ -0,0 +1,39 @@
+import discord
+from discord.embeds import Embed
+from discord.ext import commands
+import time
+import os
+from discord import FFmpegPCMAudio, PCMVolumeTransformer
+import xml.etree.ElementTree as ET
+
+
+if os.name =='nt':
+    ffmpegPath = r"C:\\FFmpeg\\bin\\ffmpeg.exe"
+else:
+    ffmpegPath = "ffmpeg"
+#
+# Radio Cog
+#
+
+class controls(commands.Cog, name="Controls"):
+
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    # Leave VC Channel
+    @commands.slash_command(description="stops and disconnects the bot from voice")
+    async def leave(self, ctx):
+        print(f"{time.strftime('%m/%d/%y %I:%M%p')} - /{ctx.command} - Server:{ctx.guild} - User:{ctx.author}")
+        await ctx.voice_client.disconnect()
+        await ctx.respond("Leaving Room!")
+
+
+    
+
+# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class.
+# When we load the cog, we use the name of the file.
+
+def setup(bot):
+   bot.add_cog(controls(bot))
