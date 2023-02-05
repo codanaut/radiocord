9e14d56a3cea0ab724f9284a997cc8287a4e004e
Codanaut <cody@codanaut.com>
2023-02-04 17:16:01 -0700

    updated connection info
    updated embed when connect to vc

--- stations/lofi.py
+++ stations/lofi.py
@@ -35,7 +35,7 @@ class lofi(commands.Cog, name="Lofi Stations"):
         connected = ctx.author.voice
         if connected:
             await connected.channel.connect()
-            connectionEmbed = Embed(title=f"Connecting to {connected.channel} and starting stream!")
+            connectionEmbed = Embed(title=f"Connecting to {connected.channel} and starting stream!", description="This may take a moment!")
             connectionEmbed.set_footer(text="(note: some live streams may go offline at times, if a stream is dead try another)")
             await ctx.respond(embed=connectionEmbed)
             ydl_opts = {
@@ -50,11 +50,12 @@ class lofi(commands.Cog, name="Lofi Stations"):
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(streamURL, download=False)
                 video_url = info['url']
+                webpage_url = info['webpage_url']
                 video_title = info['title']
                 video_id = info['id']
                 video_thumbnail = f"https://i3.ytimg.com/vi/{video_id}/maxresdefault.jpg"
                 ctx.voice_client.play(discord.FFmpegPCMAudio(video_url))
-                embed=discord.Embed(title=video_title, url=video_url, description="Lofi Girl - This playlist contains the smoothest lofi hip hop beats, perfect to help you chill or study.", color=0x2ec27e)
+                embed=discord.Embed(title=video_title, url=webpage_url, description="Lofi Girl - This playlist contains the smoothest lofi hip hop beats, perfect to help you chill or study.", color=0x2ec27e)
                 embed.set_thumbnail(url=video_thumbnail)
                 await ctx.edit(embed=embed)
 
@@ -76,7 +77,7 @@ class lofi(commands.Cog, name="Lofi Stations"):
         connected = ctx.author.voice
         if connected:
             await connected.channel.connect()
-            connectionEmbed = Embed(title=f"Connecting to {connected.channel} and starting stream!")
+            connectionEmbed = Embed(title=f"Connecting to {connected.channel} and starting stream!", description="This may take a moment!")
             connectionEmbed.set_footer(text="(note: some live streams may go offline at times, if a stream is dead try another)")
             await ctx.respond(embed=connectionEmbed)
             ydl_opts = {
@@ -91,11 +92,12 @@ class lofi(commands.Cog, name="Lofi Stations"):
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(streamURL, download=False)
                 video_url = info['url']
+                webpage_url = info['webpage_url']
                 video_title = info['title']
                 video_id = info['id']
                 video_thumbnail = f"https://i3.ytimg.com/vi/{video_id}/maxresdefault.jpg"
                 ctx.voice_client.play(discord.FFmpegPCMAudio(video_url))
-                embed=discord.Embed(title=video_title, url=video_url, description="Lofi Girl - This playlist contains the smoothest lofi hip hop beats, perfect to help you relax or fall asleep.", color=0x2ec27e)
+                embed=discord.Embed(title=video_title, url=webpage_url, description="Lofi Girl - This playlist contains the smoothest lofi hip hop beats, perfect to help you relax or fall asleep.", color=0x2ec27e)
                 embed.set_thumbnail(url=video_thumbnail)
                 await ctx.edit(embed=embed)
 
@@ -118,7 +120,7 @@ class lofi(commands.Cog, name="Lofi Stations"):
         connected = ctx.author.voice
         if connected:
             await connected.channel.connect()
-            connectionEmbed = Embed(title=f"Connecting to {connected.channel} and starting stream!")
+            connectionEmbed = Embed(title=f"Connecting to {connected.channel} and starting stream!", description="This may take a moment!")
             connectionEmbed.set_footer(text="(note: some live streams may go offline at times, if a stream is dead try another)")
             await ctx.respond(embed=connectionEmbed)
             ydl_opts = {
@@ -133,11 +135,12 @@ class lofi(commands.Cog, name="Lofi Stations"):
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(streamURL, download=False)
                 video_url = info['url']
+                webpage_url = info['webpage_url']
                 video_title = info['title']
                 video_id = info['id']
                 video_thumbnail = f"https://i3.ytimg.com/vi/{video_id}/maxresdefault.jpg"
                 ctx.voice_client.play(discord.FFmpegPCMAudio(video_url))
-                embed=discord.Embed(title=video_title, url=video_url, description="Chillhop Radio - jazzy & lofi hip hop beats", color=0x2ec27e)
+                embed=discord.Embed(title=video_title, url=webpage_url, description="Chillhop Radio - jazzy & lofi hip hop beats", color=0x2ec27e)
                 embed.set_thumbnail(url=video_thumbnail)
                 await ctx.edit(embed=embed)
 
--- stations/paddockRadio.py
+++ stations/paddockRadio.py
@@ -40,7 +40,8 @@ class paddockRadio(commands.Cog, name="Radio Commands"):
         if connected:
             await connected.channel.connect()
             ctx.voice_client.play(source, after=None)
-            connectionEmbed = Embed(title=f"Connecting to {connected.channel}")
+            connectionEmbed = Embed(title=f"Connecting to {connected.channel} and starting stream!", description="This may take a moment! Hang Tight!")
+            connectionEmbed.set_footer(text="(note: some live streams may go offline at times, if a stream is dead try another)")
             await ctx.respond(embed=connectionEmbed)
             if self.updateTask is not None:
                 self.updateTask.cancel()
